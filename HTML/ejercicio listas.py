809bac6a574b32c350e884d498d2025133986073
MARIAca02 <159548894+MARIAca02@users.noreply.github.com>
2024-02-21 10:04:27 -0500

    Add files via upload

---
+++ HTML/ejercicio listas.py
@@ -0,0 +1,17 @@
+# ejercicio_ lista
+valor_a = 5
+#               0 1 2 3 4 
+valor_lista = [4,7,8,9,10,20,30,40,55]
+print(valor_lista[8])
+#ejercico _ lista_ texto
+lista_2 = ["saludo","juan","estudiante","andrea"]
+print(lista_2[3])
+#ejercicio_listas_aplicando_append
+lista_2.append ('prueba')
+print(lista_2)
+# ejercicios_lista_aplicando_pop
+lista_2.pop()
+print(lista_2)
+# ejercicios_lista_aplicando_clear
+lista_2.clear()
+print(lista_2)
